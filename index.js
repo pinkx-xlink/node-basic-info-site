a801bea6416a221d27694b87f32ad0471fd62fd0
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-05-08 17:09:08 -0400

    Provide a fallback value to PORT variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Node.js Express app running on port ${PORT}`);
 })
