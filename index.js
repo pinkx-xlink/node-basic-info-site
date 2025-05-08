c29936a748fa5ac7a64dd71a78fcea78fb95f8a5
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-05-08 17:07:27 -0400

    Create PORT variable and run a console.log statement to test

--- index.js
+++ index.js
@@ -1,125 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => res.send("Hello, world!"));
 
-// const { createServer } = require('node:http');
-const http = require('http');
-// const path = require("path");
-const fs = require('fs');
-const EventEmitter = require('events');
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Node.js Express app running on port ${PORT}`);
+})
+// app.use(express.static('public'));
+// app.use(express.urlencoded({ extended: true }));
 
-class MyEmitter extends EventEmitter {}
-const eventEmitter = new MyEmitter();
+// app.set('view engine', 'ejs');
 
-const server = http.createServer((req, res) => {
-  // // Build file path
-  // let filePath = path.join(
-  //   __dirname,
-  //   "public",
-  //   // If there isn't a specified route, display index.html
-  //   // otherwise use the speficied .html file
-  //   req.url === "/" ? "index.html" : req.url
-  // );
-  if (req.url === '/') {
-    fs.readFile('./index.html', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Interal Server Error');
-        return;
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    });
-  } else if (req.url === '/about') {
-    eventEmitter.emit('switchPage', res);
-  } else if (req.url === '/contactMe') {
-    eventEmitter.emit('switchPage', res);
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Page Not Found');
-  }
-  // res.statusCode = 200;
-  // // Build file path
-  // let filePath = path.join(
-  //   __dirname,
-  //   "public",
-  //   // If there isn't a specified route, display index.html
-  //   // otherwise use the speficied .html file
-  //   req.url === "/" ? "index.html" : req.url
-  // );
+// const userRouter = require()
 
-  // File extension
-  // let extname = path.extname(filePath);
+// app.use('/', useRouter);
 
-  // Initial content type
-  // let contentType = "text/html";
-
-  // Check ext and set content type
-  // switch (extname) {
-  //   case ".js":
-  //       contentType = "text/javascript";
-  //       break;
-  //     case ".css":
-  //       contentType = "text/css";
-  //       break;
-  //     case ".json":
-  //       contentType = "application/json";
-  //       break;
-  //     case ".png":
-  //       contentType = "image/png";
-  //       break;
-  //     case ".jpg":
-  //       contentType = "image/jpg";
-  //       break;
-  // }
-
-  // Check if contentType is text/html but no .html file ext
-  // if (contentType == "text/html" && extname == "") filePath += ".html";
-
-  // // log the filePath
-  // console.log(filePath);
-
-  // // Read file
-  // fs.readFile(filePath, (err, content) => {
-  //   if (err) {
-  //       if (err.code == "ENOENT") {
-  //           // Page not found
-  //           fs.readFile(
-  //               path.join(__dirname, "public", "404.html"),
-  //               (err, content) => {
-  //                   res.writeHead(404, { "Content-Type": "text.html" });
-  //                   res.end(content, "utf8");
-  //               }
-  //           );
-  //       } else {
-  //           // Some server error
-  //           res.writeHead(500);
-  //           res.end(`Server Error: ${err.code}`);
-  //       }
-  //   } else {
-  //       // Success
-  //       res.writeHead(200, { "Content-Type": contentType });
-  //       res.end(content, "utf8");
-  //       eventEmitter.emit('switchPage', res);
-  //   } 
-  // });
-});
-
-
-eventEmitter.on('switchPage', (res) => {
-  console.log('started');
-  fs.readFile('about.html', (err, data) => {
-    if (err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Interal server error');
-      return;
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
-  
-});
-
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+// app.listen(3000);
