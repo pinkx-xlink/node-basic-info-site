ed36d4fb6b89ebc9d41f94b2a79f460fab59dbb4
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-05-08 17:43:23 -0400

    Create About Page redirect

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/resend-html", (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-const PORT = process.env.PORT || 3000;
+app.get("/about", function (req, res) {
+  res.send("Redirected to About Page");
+})
+
+
 app.listen(PORT, () => {
   console.log(`Node.js Express app running on port ${PORT}`);
 })
