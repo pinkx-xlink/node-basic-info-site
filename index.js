25757f66c002cb82ea54266c4f31b792eec41614
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-05-08 17:56:20 -0400

    Use app.get method to create a function that redirects to Contact Me page when the button is clicked

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/about", function (req, res) {
   res.send("Redirected to About Page");
 })
 
+app.get("/contactMe", function (req, res) {
+  res.send("Redirected to Contact Me Page");
+})
+
 
 app.listen(PORT, () => {
   console.log(`Node.js Express app running on port ${PORT}`);
