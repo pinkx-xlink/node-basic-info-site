cebb3a92e1206903947232e79c6736c8868e165f
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-05-08 20:10:08 -0400

    Comment out lines 13-23 (the former /, About, and Contact Me pages); fix typos lines 3-5

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const express = require("express");
 const app = express();
-const authorRouter = require("./routes/authorRouther");
-const bookRouter = require("./routes/bookRouther");
-const indexRouter = require("./routes/indexRouther");
+const authorRouter = require("./routes/authorRouter");
+const bookRouter = require("./routes/bookRouter");
+const indexRouter = require("./routes/indexRouter");
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
+
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// app.get("/about", function (req, res) {
+//   res.send("Redirected to About Page");
+// })
+
+// app.get("/contactMe", function (req, res) {
+//   res.send("Redirected to Contact Me Page");
+// })
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get("/about", function (req, res) {
-  res.send("Redirected to About Page");
-})
-
-app.get("/contactMe", function (req, res) {
-  res.send("Redirected to Contact Me Page");
-})
-
-
 app.listen(PORT, () => {
   console.log(`Node.js Express app running on port ${PORT}`);
 })
