a367fc6be52c3f72fee391aba7d037a5bc7e87f3
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-05-30 10:54:26 -0400

    Install and utilize express async handler

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,16 +1,19 @@
+const asyncHandler = require("express-async-handler");
 const db = require("../db");
 
-async function getAuthorById(req, res) {
+// Function will automatically catch any errors thrown thanks to express async handler
+
+const getAuthorById = asyncHandler(async (req, res) => {
     const { authorId } = req.params;
 
     const author = await db.getAuthorById(Number(authorId));
 
-    if (!author) {
-        res.status(404).send("Author not found");
-        return;
-    }
-
+        if (!author) {
+            res.status(404).send("Author not found");
+            return;
+        }
+        
     res.send(`Author name: ${author.name}`);
-};
+});
 
 module.exports = { getAuthorById };
--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -7,7 +7,7 @@ const authorRouter = Router();
 authorRouter.get("/", (req, res) => res.send("All authors"));
 authorRouter.get("/:authorId", (req, res) => {
     const { authorId } = req.params;
-    res.send(`Author ID: ${authorId}`);
+    res.send(`Author ID is: ${authorId}`);
 });
 
 authorRouter.get("/:authorId", getAuthorById);
