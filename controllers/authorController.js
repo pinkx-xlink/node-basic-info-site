06880c069f2328e10e1d39d7e290772ea6deae32
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-05-30 11:05:02 -0400

    Import CustomNotFoundError and use it in getAuthorById when there is no author found with the id input

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
 
 // Function will automatically catch any errors thrown thanks to express async handler
 
@@ -9,10 +10,9 @@ const getAuthorById = asyncHandler(async (req, res) => {
     const author = await db.getAuthorById(Number(authorId));
 
         if (!author) {
-            res.status(404).send("Author not found");
-            return;
+            throw new CustomNotFoundError("Author not found!");
         }
-        
+
     res.send(`Author name: ${author.name}`);
 });
 
