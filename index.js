f8dacad41eba7b92dd9e024bb94a5c52760e490a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-05-30 11:10:29 -0400

    Refactor the error handler to use 500 as a fallback if no other statusCode is provided

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.use("/", indexRouter);
 // Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send(err);
+  // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+  res.status(err.statusCode || 500).send(err.message);
 });
 
 
