9d3fe29b3eaf2404a3012acfd1dda3d8d37a4725
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-05-30 13:52:20 -0400

    Create views dolfer and add path variable to use "views"

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
 
+const path = require("node:path");
+
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", ejs);
 
 // app.get("/", (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
